8d9f03d4aa9964b8c94105e5033244f1e19546d1
samirahmad5445 <samirahmad5445@gmail.com>
2023-11-02 02:06:08 +0200

    change the color theme

--- src/App.js
+++ src/App.js
@@ -19,16 +19,16 @@ export default function App() {
   }, []);
 
   return (
-    <div className="min-h-screen py-8 bg-slate-950 text-emerald-50 grid place-content-center">
-      <div className="max-w-lg  bg-slate-700 p-8 m-6 rounded-lg border border-emerald-400 shadow-md shadow-slate-800">
-        <h1 className="text-3xl md:text-4xl xl:text-5xl text-center font-bold italic mb-8 before:content-[open-quote] after:content-[close-quote] before:text-emerald-400 after:text-emerald-400">
+    <div className="min-h-screen py-8 bg-slate-950 text-sky-50 grid place-content-center">
+      <div className="max-w-lg  bg-slate-700 p-8 m-6 rounded-lg border border-sky-400 shadow-md shadow-slate-800">
+        <h1 className="text-3xl md:text-4xl xl:text-5xl text-center font-bold italic mb-8 before:content-[open-quote] after:content-[close-quote] before:text-sky-400 after:text-sky-400">
           {advice}
         </h1>
         <div className="flex justify-between items-center gap-8">
           <AdviceCount count={count} />
           <button
             onClick={getAdvice}
-            className="px-6 py-2 border-2 border-emerald-600 hover:bg-emerald-500 hover:border-emerald-500 font-bold rounded-full transition-colors duration-300"
+            className="px-6 py-2 border-2 border-sky-600 hover:bg-sky-500 hover:border-sky-500 font-bold rounded-full transition-colors duration-300"
           >
             Get Advice &rarr;
           </button>
@@ -41,7 +41,7 @@ export default function App() {
 const AdviceCount = ({ count }) => {
   return (
     <p className="text-slate-400">
-      Advice Count: <span className="text-emerald-400 font-bold">{count}</span>
+      Advice Count: <span className="text-sky-400 font-bold">{count}</span>
     </p>
   );
 };
